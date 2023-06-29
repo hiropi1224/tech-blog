23838afbf584e2c612bddf1a0ad4e8632c976ed0
Shirahamah <hiropi1224s@gmail.com>
2023-06-29 23:31:26 +0900

    💄 ルートのlayout、pageにデータ取得処理追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,24 @@
-export default function Home() {
+import { getList } from '@/app/_libs/microcms';
+
+export default async function Home() {
+  const data = await getList();
+
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+    <main className='flex min-h-screen flex-col items-center'>
       Hello, world!
+      <div className='flex flex-col'>
+        {data.contents.map((content) => (
+          <div key={content.id} className='border-b border-mauve-2'>
+            <div>{content.title}</div>
+            <div>{content.createdAt}</div>
+            <div>
+              {content.tags?.map((tag) => (
+                <p key={tag.id}>{tag.name}</p>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
