b70ef71c651ba6f34637ccbaddb9d1f80522ea73
Shirahamah <hiropi1224s@gmail.com>
2023-07-01 20:31:11 +0900

    tag-itemのstyle修正

--- src/app/_components/tag-item/tag-item.tsx
+++ src/app/_components/tag-item/tag-item.tsx
@@ -4,7 +4,7 @@ import { tv } from 'tailwind-variants';
 import { Tag } from '@/app/_libs/microcms';
 
 const tagStyle = tv({
-  base: 'whitespace-nowrap rounded-sm bg-mauvedark-5 px-2 py-1 text-amber-1',
+  base: 'whitespace-nowrap rounded-sm bg-mauve-10 px-2 py-1 text-sm text-amber-1',
 });
 
 type Props = {
