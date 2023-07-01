5eb3ca5a364bd0d7133f6745fcaa6d699b7f922a
Shirahamah <hiropi1224s@gmail.com>
2023-07-01 16:25:20 +0900

    💄 style修正

--- src/app/_components/tag-item/tag-item.tsx
+++ src/app/_components/tag-item/tag-item.tsx
@@ -1,6 +1,11 @@
 import Link from 'next/link';
+import { tv } from 'tailwind-variants';
 
 import { Tag } from '@/app/_libs/microcms';
+
+const tagStyle = tv({
+  base: 'whitespace-nowrap rounded-sm bg-mauvedark-5 px-2 py-1 text-amber-1',
+});
 
 type Props = {
   tag: Tag;
@@ -9,7 +14,7 @@ type Props = {
 export const TagItem: React.FC<Props> = ({ tag }) => {
   return (
     <Link href={`/tags/${tag.id}`}>
-      <div className='whitespace-nowrap rounded-sm bg-mauve-5 px-2 py-1'>{`#${tag.name}`}</div>
+      <div className={tagStyle()}>{`#${tag.name}`}</div>
     </Link>
   );
 };
--- src/app/_components/tag-list/tag-list.tsx
+++ src/app/_components/tag-list/tag-list.tsx
@@ -1,3 +1,5 @@
+import { tv } from 'tailwind-variants';
+
 import { TagItem } from '@/app/_components/tag-item';
 import { Tag } from '@/app/_libs/microcms';
 
@@ -6,13 +8,17 @@ type Props = {
   hasLink?: boolean;
 };
 
+const tagListStyle = tv({
+  base: 'mx-2 flex flex-wrap gap-2',
+});
+
 export const TagList: React.FC<Props> = ({ tags }) => {
   if (!tags) {
     return null;
   }
 
   return (
-    <ul className='mx-2 flex flex-wrap gap-2'>
+    <ul className={tagListStyle()}>
       {tags.map((tag) => (
         <li key={tag.id}>
           <TagItem tag={tag} />
