6f69b1c116a3902a1a0d19bde1c8fa3e74ce4abd
Shirahamah <hiropi1224s@gmail.com>
2023-07-03 23:05:28 +0900

    ♻️

--- src/app/(without-side)/layout.tsx
+++ src/app/(without-side)/layout.tsx
@@ -14,7 +14,7 @@ export const metadata = {
 
 export default async function RootLayout(props: { children: React.ReactNode }) {
   return (
-    <main className='m-auto flex flex-col bg-mauve-3 lg:grid lg:grid-cols-6 lg:gap-4'>
+    <main>
       <div className='lg:col-span-4 lg:col-start-2'>{props.children}</div>
     </main>
   );
