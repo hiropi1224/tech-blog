9578f22bb34c0e2530a7c1c64dacd752980e45ab
Shirahamah <hiropi1224s@gmail.com>
2023-07-05 00:10:51 +0900

    ♻️ style調整

--- src/app/(with-side)/layout.tsx
+++ src/app/(with-side)/layout.tsx
@@ -1,10 +1,12 @@
+import { tv } from 'tailwind-variants';
+
 export const metadata = {
   metadataBase: new URL(process.env.BASE_URL || 'http://localhost:3000'),
   title: 'Tech Blog',
-  description: 'Tech Blog presented by microCMS',
+  description: 'Technology Blog by shirahama',
   openGraph: {
     title: 'Tech Blog',
-    description: 'Tech Blog presented by microCMS',
+    description: 'Technology Blog by shirahama',
     images: '/ogp.png',
   },
   alternates: {
@@ -12,14 +14,24 @@ export const metadata = {
   },
 };
 
+const layoutStyle = tv({
+  slots: {
+    base: 'm-auto mt-4 flex flex-col gap-4 bg-mauve-3 lg:grid lg:grid-cols-4 lg:gap-4',
+    main: 'm-2 lg:col-span-3',
+    side: 'm-2 lg:col-span-1',
+  },
+});
+
+const { base, main, side } = layoutStyle();
+
 export default async function Layout(props: {
   children: React.ReactNode;
   side: React.ReactNode;
 }) {
   return (
-    <main className='m-auto flex flex-col bg-mauve-3 lg:grid lg:grid-cols-4 lg:gap-4'>
-      <div className='lg:col-span-3'>{props.children}</div>
-      <div className='lg:col-span-1'>{props.side}</div>
+    <main className={base()}>
+      <div className={main()}>{props.children}</div>
+      <div className={side()}>{props.side}</div>
     </main>
   );
 }
